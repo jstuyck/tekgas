d669453a4384d8e6e19bfdbb17c1ebf061efe12d
Julien Stuyck <stuyck@gmail.com>
2018-05-30 10:30:52 +0200

    Add missing files

---
+++ src/app/modules/tools/components/trimix/trimix.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { DiveConstant } from '@app/modules/shared/models/constant';
+
+@Component({
+    // tslint:disable-next-line:component-selector
+    selector: 'tool-trimix',
+    templateUrl: './trimix.component.html',
+    styleUrls: ['./trimix.component.scss']
+})
+export class TrimixComponent implements OnInit {
+
+    ngOnInit() {
+    }
+}
